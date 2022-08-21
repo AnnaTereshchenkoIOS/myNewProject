515a553a44247f9590e60f41c36be50ca0bcfeff
Anna Tereshchenko <annatere.developer@gmail.com>
2022-08-21 14:04:00 +0300

    Initial Commit

---
+++ myNewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myNewProject
+//
+//  Created by Anna on 21.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
